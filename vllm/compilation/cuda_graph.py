b0b044ae0d962432334a3c8d99f85a0ff02e4f08
Joseph Groenenboom <jgroenen@redhat.com>
2025-10-08 22:23:27 +0000

    Enable checking for cuda graph input address changes via env var

--- vllm/compilation/cuda_graph.py
+++ vllm/compilation/cuda_graph.py
@@ -61,7 +61,8 @@ class CUDAGraphWrapper:
     assumption on the dynamic shape (batch size) of the runtime inputs, as a
     trade-off for staying orthogonal to compilation logic. Nevertheless,
     tracing and checking the input addresses to be consistent during replay is
-    guaranteed when VLLM_LOGGING_LEVEL == "DEBUG".
+    guaranteed when VLLM_LOGGING_LEVEL == "DEBUG" or by setting 
+    VLLM_TORCH_COMPILE_DEBUG == TRUE.
     """
 
     def __init__(
@@ -77,7 +78,7 @@ class CUDAGraphWrapper:
         self.compilation_config = vllm_config.compilation_config
 
         self.first_run_finished = False
-        self.is_debugging_mode = envs.VLLM_LOGGING_LEVEL == "DEBUG"
+        self.is_debugging_mode = envs.VLLM_LOGGING_LEVEL == "DEBUG" or envs.VLLM_TORCH_COMPILE_DEBUG
 
         # assert runtime_mode is not NONE(no cudagraph), otherwise, we don't
         # need to initialize a CUDAGraphWrapper.
